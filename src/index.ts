d6c77abab05930e697add62c541cf5bd3b0a7090
Hellicat <jhccop@naver.com>
2024-03-23 15:43:02 +0900

    install graphql ts to start

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {ApolloServer} from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone";
+
+const server = new ApolloServer({});
+
+const {url} = await startStandaloneServer(server, {listen: {port: 4000}});
+console.log(`Server is Listening on ${url}`);
