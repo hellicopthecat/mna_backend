52f2f22f6ee06993e665889c4c1979cc37c24bbc
Hellicat <jhccop@naver.com>
2024-03-25 21:20:34 +0900

    Mutation login user

---
+++ src/user/loginUser/loginUser.resolvers.ts
@@ -0,0 +1,44 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {User} from "@prisma/client";
+import client from "../../prismaClient";
+
+export default {
+  Mutation: {
+    loginUser: async (_: any, {email, password}: User) => {
+      try {
+        const existsUser = await client.user.findFirst({where: {email}});
+        if (!existsUser) {
+          return {
+            ok: false,
+            errorMsg: "가입된 이메일계정이 존재하지 않습니다.",
+          };
+        }
+        const confirmPassword = await bcrypt.compare(
+          password,
+          existsUser.password
+        );
+        if (!confirmPassword) {
+          return {
+            ok: false,
+            errorMsg: "비밀번호가 일치하지 않습니다.",
+          };
+        }
+        const token = jwt.sign(
+          {
+            id: existsUser.id,
+            username: existsUser.username,
+            email: existsUser.email,
+          },
+          process.env.TOKEN_SECRET_KEY
+        );
+        return {
+          ok: true,
+          token,
+        };
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
