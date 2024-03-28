d5b545372afc672cfc5770f0ec8ba17c7db0d31c
Hellicat <jhccop@naver.com>
2024-03-28 22:20:12 +0900

    create edit company adress types & resolvers

---
+++ src/company/editCompany/editCompany.resolvers.ts
@@ -0,0 +1,59 @@
+import {ComapnyAdress, Company} from "@prisma/client";
+import {protectResolver} from "../../user/user.util";
+import client from "../../prismaClient";
+
+export default {
+  Mutation: {
+    editCompanyAdress: protectResolver(
+      async (
+        _,
+        {
+          companyName,
+          country,
+          city,
+          streetAdress,
+          restAdress,
+          adressNum,
+        }: Company & ComapnyAdress
+      ) => {
+        try {
+          //Exists Company
+          const existsCompany = await client.company.findFirst({
+            where: {companyName},
+          });
+          if (!existsCompany) {
+            return {
+              ok: false,
+              errorMsg: "조회하신 회사는 존재하지 않습니다.",
+            };
+          }
+          const updateAdress = await client.company.update({
+            where: {id: existsCompany.id},
+            data: {
+              companyAdress: {
+                update: {
+                  country,
+                  city,
+                  streetAdress,
+                  restAdress,
+                  adressNum,
+                },
+              },
+            },
+          });
+          if (!updateAdress) {
+            return {
+              ok: false,
+              errorMsg: "주소 편집에 실패했습니다.",
+            };
+          }
+          return {
+            ok: true,
+          };
+        } catch (err) {
+          return err;
+        }
+      }
+    ),
+  },
+};
