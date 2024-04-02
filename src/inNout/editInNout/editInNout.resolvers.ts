2eb727a9d1b5167261c94aacae9c20b8019c0760
Hellicat <jhccop@naver.com>
2024-04-02 21:53:00 +0900

    create editInNout

---
+++ src/inNout/editInNout/editInNout.resolvers.ts
@@ -0,0 +1,62 @@
+import {Company, InNout} from "@prisma/client";
+import {protectResolver} from "../../user/user.util";
+import client from "../../prismaClient";
+
+export default {
+  Mutation: {
+    editInNout: protectResolver(
+      async (
+        _,
+        {
+          companyName,
+          capitalStock,
+          budget,
+          accountNum,
+          accountName,
+        }: Company & InNout,
+        {logginUser}
+      ) => {
+        const {
+          companyName: existsCompany,
+          companyManager,
+          inNoutId,
+        } = await client.company.findUnique({
+          where: {companyName, companyManager: {some: {id: logginUser.id}}},
+          select: {companyName: true, companyManager: true, inNoutId: true},
+        });
+        if (!existsCompany) {
+          return {
+            ok: false,
+            errorMsg: "회사가 존재하지 않습니다.",
+          };
+        }
+        companyManager.map((manager) => {
+          if (manager.id !== logginUser.id) {
+            return {
+              ok: false,
+              errorMsg: "메니저로 등록되지 않습니다.",
+            };
+          }
+        });
+        const updateInNout = await client.inNout.update({
+          where: {id: inNoutId},
+          data: {
+            capitalStock,
+            budget,
+            accountNum,
+            accountName,
+          },
+        });
+        if (!updateInNout) {
+          return {
+            ok: false,
+            errorMsg: "회사 자산을 업데이트할 수 없습니다.",
+          };
+        }
+        return {
+          ok: true,
+        };
+      }
+    ),
+  },
+};
