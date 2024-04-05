4a71f3f3cf8362ea227174b25d2727c888802b68
Hellicat <jhccop@naver.com>
2024-04-05 22:06:19 +0900

    add more typeDefs Obj & add more updateModels

--- src/incomeExpend/updateInEx/updateInEx.resolvers.ts
+++ src/incomeExpend/updateInEx/updateInEx.resolvers.ts
@@ -1,4 +1,4 @@
-import {Company, IncomeExpend} from "@prisma/client";
+import {Company, EquityLiabilities, InNout, IncomeExpend} from "@prisma/client";
 import client from "../../prismaClient";
 import {Resolvers} from "../../types";
 import {protectResolver} from "../../user/user.util";
@@ -11,6 +11,7 @@ export default {
         {
           companyName,
           infoSubtitle,
+          money,
           incomeTrue,
           paymentsDone,
         }: Company & IncomeExpend,
@@ -28,6 +29,12 @@ export default {
         }
         const existsInEx = await client.incomeExpend.findUnique({
           where: {infoSubtitle},
+          select: {
+            inNoutId: true,
+            money: true,
+            paymentsDone: true,
+            incomeTrue: true,
+          },
         });
         if (!existsInEx) {
           return {
@@ -35,24 +42,52 @@ export default {
             errorMsg: "존재하지 않는 거래입니다.",
           };
         }
+
+        if (existsInEx.paymentsDone === "PAID") {
+          return {
+            ok: false,
+            errorMsg: "지불이 완료된 거래입니다.",
+          };
+        }
+
         const updateInEx = await client.incomeExpend.update({
           where: {infoSubtitle},
           data: {
             incomeTrue,
             paymentsDone,
-            InNout: {
-              update: {
-                budget: {
-                  increment:
-                    incomeTrue && paymentsDone ? existsInEx.money : null,
-                  decrement:
-                    !incomeTrue && paymentsDone ? existsInEx.money : null,
-                },
-              },
-            },
+            money,
           },
         });
-        if (!updateInEx) {
+        const updateEnL = await client.equityLiabilities.update({
+          where: {enLId: infoSubtitle},
+          data: {
+            value: money,
+          },
+        });
+        let updateBudget: any;
+        const income = existsInEx.incomeTrue && paymentsDone === "PAID";
+        const expend = !existsInEx.incomeTrue && paymentsDone === "PAID";
+        if (income) {
+          updateBudget = await client.inNout.update({
+            where: {id: existsInEx.inNoutId},
+            data: {
+              budget: {
+                increment: existsInEx.money,
+              },
+            },
+          });
+        } else if (expend) {
+          updateBudget = await client.inNout.update({
+            where: {id: existsInEx.inNoutId},
+            data: {
+              budget: {
+                decrement: existsInEx.money,
+              },
+            },
+          });
+        }
+        const NOT_UPDATE = !updateInEx || !updateEnL || !updateBudget;
+        if (NOT_UPDATE) {
           return {
             ok: false,
             errorMsg: "업데이트에 실패하였습니다.",
