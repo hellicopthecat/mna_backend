03408cf4d65e5e5df3ea27cf24c693b2cc10e0d0
Hellicat <jhccop@naver.com>
2024-04-17 22:58:55 +0900

    create cancelRegistWorker

---
+++ src/company/cancelRegistWorker/cancelRegistWorker.resolvers.ts
@@ -0,0 +1,30 @@
+import {Company, User} from "@prisma/client";
+import {protectResolver} from "../../user/user.util";
+import client from "../../prismaClient";
+
+export default {
+  Mutation: {
+    cancelRegistWorker: protectResolver(
+      async (_, {username, companyName}: User & Company, {logginUser}) => {
+        const checkAdmin = await client.company.findFirst({
+          where: {companyName, companyManager: {some: {id: logginUser.id}}},
+        });
+        if (!checkAdmin) {
+          return {ok: false, errorMsg: "권한이 없습니다."};
+        }
+        const existsWorker = await client.user.findFirst({where: {username}});
+        if (!existsWorker) {
+          return {ok: false, errorMsg: "유저가 존재하지 않습니다."};
+        }
+        const registWorkerResult = await client.company.update({
+          where: {companyName},
+          data: {worker: {disconnect: {id: existsWorker.id}}},
+        });
+        if (!registWorkerResult) {
+          return {ok: false, errorMsg: "직원 등록 해제가 실패했습니다."};
+        }
+        return {ok: true};
+      }
+    ),
+  },
+};
